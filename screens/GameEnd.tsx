136e878bec51aed1e018c7be4b5e0e49ebd1a1f8
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-24 22:12:22 +0530

    add: added initial styles for game_over

--- screens/GameEnd.tsx
+++ screens/GameEnd.tsx
@@ -1,12 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/Title";
+import Colors from "../constants/colors";
 
 const GameEnd: React.FC = () => {
   return (
-    <View>
-      <Text>Game End</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/success.png")}
+          resizeMode="cover"
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X round to guess the number Y</Text>
     </View>
   );
 };
 
 export default GameEnd;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    height: 300,
+    borderRadius: 200,
+    width: 300,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    margin: 60,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
