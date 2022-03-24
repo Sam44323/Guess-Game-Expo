408a80ee838a4cb486018297e1cc4b068a9b0a03
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-24 17:37:48 +0530

    add: added initial struture for primary-button

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const PrimaryButton: React.FC = () => {
-  return <></>;
+const PrimaryButton: React.FC = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
 };
 
 export default PrimaryButton;
--- screens/StartGame.tsx
+++ screens/StartGame.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGame: React.FC = () => {
-  return <></>;
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 };
 
 export default StartGame;
